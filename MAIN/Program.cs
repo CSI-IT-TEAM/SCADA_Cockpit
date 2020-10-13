64dfdf300318f61a14b9cf6a566776a9ca16157b
DO.IT <DO.IT@VJ-V-IT-DO>
2020-10-13 11:05:46 +0700

    UPDATE COMVAR

--- MAIN/Program.cs
+++ MAIN/Program.cs
@@ -15,7 +15,7 @@ namespace MAIN
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            Application.Run(new Main());  
          //   Application.Run(new FormFlash());
         }
     }
